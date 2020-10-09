8b4704c3feba914713d40ceab7a58de1777c3b81
imran <imran.cse016@outlook.com>
2020-10-09 03:55:44 -0700

    Testing from laptop

--- Api/FreeApiTesting.cs
+++ Api/FreeApiTesting.cs
@@ -19,13 +19,13 @@ namespace BlazorApp.Api
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string name =  "Azure function -" + req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
+            string responseMessage =  string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
